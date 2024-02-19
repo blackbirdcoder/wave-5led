c070272711497857cd9868749efc80f44922235c
Eugene-TechnoNinja <technoninja@ukr.net>
2024-02-19 13:26:02 +0200

    code refactoring

--- sketch_run_wave_5led/sketch_run_wave_5led.ino
+++ sketch_run_wave_5led/sketch_run_wave_5led.ino
@@ -2,25 +2,22 @@
 
 int period = 200;
 
+void ledShow(byte pin, int millisec) {
+  digitalWrite(pin, HIGH);
+  delay(millisec);
+  digitalWrite(pin, LOW);
+  delay(millisec);
+}
+
 void setup() {
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
+  for (byte i = 2; i <= 6; i++)
+    pinMode(i, OUTPUT);
 }
 
 void loop() {
-  for (int i = 2; i < 7; i++) {
-    digitalWrite(i, HIGH);
-    delay(period);
-    digitalWrite(i, LOW);
-    delay(period);
-  }
-  for (int i = 5; i > 2; i--) {
-    digitalWrite(i, HIGH);
-    delay(period);
-    digitalWrite(i, LOW);
-    delay(period);
-  } 
+  for (byte i = 2; i < 7; i++)
+    ledShow(i, period);
+
+  for (byte i = 5; i > 2; i--)
+    ledShow(i, period);
 }
